7e27e114f4156ae1c27e4749ff856820b6d6262d
DarkMio <miostarkid@gmail.com>
2015-04-30 21:33:57 +0200

    cleanup

---
+++ autoclicker/AutoClicker.java
@@ -0,0 +1,13 @@
+package autoclicker;
+
+import java.awt.AWTException;
+
+/**
+ * The main program for the autoclicker.
+ */
+public class AutoClicker {
+    public static void main(String[] args) throws AWTException {
+        Click click = new Click(true);
+        click.clicker();
+    }
+}
